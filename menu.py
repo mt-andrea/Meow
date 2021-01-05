b9120a0f4b4e5aa6f538f5658d0201b99483fa14
mt-andrea <mateandicica@gmail.com>
2021-01-05 18:01:26 +0100

    I like it waitin for coms find sometg u dont like

--- menu.py
+++ menu.py
@@ -22,7 +22,7 @@ def main():
             if choise=="1":
                 print_data(filename)
             elif choise=="2":
-                append_file(filename)
+                append_file(filename,"")
             elif choise=="3":
                 search_in_file(filename)
             elif choise=="4":
@@ -39,7 +39,8 @@ def listing_files():
             print(f)
 
 def filename_please():
-    filename=input("Kérem egy fájl nevét, amivel dolgozni fogunk (*.txt kérem a kiterjesztést is): ")
+    filename=input("Kérem egy fájl nevét, amivel dolgozni fogunk: ")
+    filename=filename+".txt"
     if not os.path.exists(filename):
         open(filename,'a').close()
     return filename
@@ -50,7 +51,7 @@ def print_data(filename):
     print(data)
     f.close()
 
-def append_file(filename,name=""):
+def append_file(filename,name):
     lista=[]
     inputFile(filename,lista)
     f=open(filename,"a")
@@ -58,7 +59,7 @@ def append_file(filename,name=""):
         name=input("Kérem egy hegység nevét: ")
         exist=name_exist(lista,name)
         while exist:
-            name=input("Kérem egy másik hegység nevét: ")
+            name=input("Az imént beírt név már szerepel adatbázisunkban. \nKérem egy másik hegység nevét: ")
             exist=name_exist(lista,name)
     hight=input("Kérem a legmagasabb csúcs magasságát (m): ")
     f.write(name+";"+hight+"\n")
@@ -66,48 +67,58 @@ def append_file(filename,name=""):
     f.close()
 
 def search_in_file(filename):
-    print("\t1 - Legalacsonyabb hegység \n\t2 - Legmagasabb hegység \n\t3 - Hegység neve \n\t4 - Magasság")
+    print("\t1 - Legalacsonyabb hegység \n\t2 - Legmagasabb hegység \n\t3 - Hegység neve \n\t4 - Magasság \n\t5 - Átlag magasság")
     choise=input("Mit keressünk? ")
     lista=[]
     inputFile(filename,lista)
     if choise=="1":
-        search_min(lista)
+        txt=search_min(lista)
     elif choise=="2":
-        search_max(lista)
+        txt=search_max(lista)
     elif choise=="3":
-        txt="Adja meg a kivánt hegység nevét ékezet nélkül (Pl.: Mount Everest): "
-        name=input("\t"+txt)
-        search_name(lista,name,filename)
+        prompt="Adja meg a kivánt hegység nevét ékezet nélkül (Pl.: Mount Everest): "
+        name=input("\t"+prompt)
+        txt=search_name(lista,name,filename)
     elif choise=="4":
-        search_hight(lista)
+        txt=search_hight(lista)
+    elif choise=="5":
+        txt=average(lista)
+    write=input("Szereté fájlba írni a keresés eredményét? (igen/nem)")
+    if write=="igen":
+       write_file(txt)
 
 def search_min(lista):
     minindex=0
     for i in range(len(lista)):
         if(lista[i][1]<=lista[minindex][1]):
             minindex=i
-    print(lista[minindex])
-
+    txt="Az adatbázis legalacsonyabb hegye: "+str(lista[minindex])
+    print("\t"+txt)
+    return txt
+    
 def search_max(lista):
     maxindex=0
     for i in range(len(lista)):
         if(lista[i][1]>=lista[maxindex][1]):
             maxindex=i
-    print(lista[maxindex])
-
+    txt="Az adatbázis legalacsonyabb hegye: "+str(lista[maxindex])
+    print("\t"+txt)
+    return txt
+    
 def search_name(lista,name,filename): 
     for i in range(0,len(lista),1):
         if(name==lista[i][0]):
             exist=True
-            print(lista[i])
+            txt="A keresett névnek megfelelő adatok: "+str(lista[i])
+            print("\t"+txt)
             break
         else:
             exist=False
     if not exist:
         append=input("Szeretné hozzáadni? (igen/nem)")
         if append=="igen":
-            append_file(filename,name)
-
+            append_file(filename,"")
+    return txt
 
 def name_exist(lista,name): 
     for i in range(0,len(lista),1):
@@ -121,11 +132,11 @@ def name_exist(lista,name):
 def search_hight(lista):
     smaler=[]
     larger=[]
-    txt="Adja meg a kívánt magasságot méterben: "
-    value=int(input("\t"+txt))
+    prompt="Adja meg a kívánt magasságot méterben: "
+    value=int(input("\t"+prompt))
     for i in range(0,len(lista),1):
         if(value==lista[i][1]):
-            print(lista[i])
+            txt="A keresett magasságnak megfelelő adatok: "+str(lista[i])
         elif value<lista[i][1]:
             larger.append(lista[i])
 
@@ -137,15 +148,25 @@ def search_hight(lista):
         for ind in range(len(smaler)):
             if smaler[ind][1]>smaler[S_ind][1]:
                 S_ind=ind
-        txt="Nem találtunk a keresésnek megfelelő elemet, de itt a hozzá legközelebb eső kisebb értékű: "+smaler[S_ind][0]+": "+str(smaler[S_ind][1])+" m"
-        print("\t"+txt)
+        txt1="Nem találtunk a keresésnek megfelelő elemet, de itt a hozzá legközelebb eső kisebb értékű: "+smaler[S_ind][0]+": "+str(smaler[S_ind][1])+" m \n"
         L_ind=0
         for ind in range(len(larger)):
             if larger[ind][1]<larger[L_ind][1]:
                 L_ind=ind
-        txt="és a hozzá legközelebb eső nagyobb értékű: "+larger[L_ind][0]+": "+str(larger[L_ind][1])+" m"
-        print("\t"+txt)
+        txt2="és a hozzá legközelebb eső nagyobb értékű: "+larger[L_ind][0]+": "+str(larger[L_ind][1])+" m"
+        txt=txt1+txt2
+    print("\t"+txt)
+    return txt 
 
+def average(lista):
+    szumm=0
+    for i in range(0,len(lista),1): 
+        szumm=szumm+lista[i][1]
+    avg=szumm/len(lista)
+    txt="Az adatbázis hegységeinek átlag magassága: "+str(round(avg,2))
+    print("\t"+txt)
+    return txt
+    
 def inputFile(filename,lista):
     f = open(filename,"r")
     for sor in f:
@@ -153,4 +174,9 @@ def inputFile(filename,lista):
         lista.append([str(sor[0]),int(sor[1])])
     f.close()
 
+def write_file(txt):
+    filename=filename_please()
+    f=open(filename,"a")
+    f.write(txt+"\n")
+    print("Keresés mentve.")
 main()
